c5dca7623b801579be7204323d458e821f05bff3
bbjxhn <jm.trinidad199@gmail.com>
2024-06-24 21:39:35 -0400

    thirteenth commit 2

--- app.js
+++ app.js
@@ -9,12 +9,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const dashboardRouter = require('./src/routes/dashboard');
 const cameraRouter = require('./src/routes/camera');
-const keypadRouter = require('./src/routes/reset-keypad');
 const passwordGenerator = require('./src/routes/password-generator');
 
 app.use('/', dashboardRouter);
 app.use('/', cameraRouter);
-app.use('/', keypadRouter); 
 app.use('/', passwordGenerator);
 
 module.exports = app;
